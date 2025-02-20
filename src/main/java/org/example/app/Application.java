60fde4d80fe1d02d82b1fed6cf76de70d802931a
Mariana Agudelo Salazar <magusa2004@gmail.com>
2025-02-20 15:38:54 -0500

    tags version

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,11 +1,12 @@
 package org.example.app;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
-    private static final ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    private static final ApplicationContext context = new AnnotationConfigApplicationContext("org.example"); //Paquete raiz
 
     public static ApplicationContext getContext() {
         return context;
